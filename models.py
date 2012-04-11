092e079ff4e41e4616c7a92a499b2999cdab56e1
daniel Sauerbrun <dsauerbrun11@ucsbalum.com>
2012-04-11 00:11:33 -0700

    safe html

--- models.py
+++ models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils.safestring import mark_safe
 
 class Blog(models.Model):
 	title = models.CharField(max_length=32)
 	date = models.DateTimeField(auto_now_add=True)
 	text = models.TextField()
 	isPrivate= models.BooleanField(default=False)
+	def displayText(self):
+		return mark_safe(self.text)
 	def __unicode__(self):
 		return u'%s' % ( self.title)	
 class Image(models.Model):
