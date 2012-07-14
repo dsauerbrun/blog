ffce0f011fe8bf0729d27a6523b464de20e3f422
Daniel Sauerbrun <dsauerbrun11@ucsbalum.com>
2012-07-14 00:42:15 -0400

    added posting folder

---
+++ posting/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.safestring import mark_safe
+from django.contrib import admin
+
+class Blog(models.Model):
+	title = models.CharField(max_length=32)
+	date = models.DateTimeField(auto_now_add=True)
+	text = models.TextField()
+	isPrivate= models.BooleanField(default=False)
+	def displayText(self):
+		return mark_safe(self.text)
+	def __unicode__(self):
+		return u'%s' % ( self.title)	
+class Comment(models.Model):
+	text = models.TextField(null=False)
+	date = models.DateTimeField(auto_now_add=True)
+	user = models.CharField(max_length=20)
+	belongs_to_blog = models.ForeignKey('Blog')
+
+class Image(models.Model):
+	image_field = models.ImageField(upload_to='uploads/')
+	belongs_to_blog = models.ForeignKey('Blog')
+	title = models.CharField(max_length=32)
+	def __unicode__(self):
+		return u'%s' % ( self.title)	
+
---
+++ posting/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from posting.models import Blog
+from posting.models import Image
+from posting.models import Comment
+from django.forms import ModelForm,Textarea,TextInput
+import os
+
+def index(request):
+	if request.user.is_staff:
+		entries = Blog.objects.all()
+	else:
+		entries = Blog.objects.filter(isPrivate=False)
+	entries = entries.order_by('-date')
+	for entry in entries:
+		entry.text=entry.text.replace('\n','<br />')	
+	images = Image.objects.all()
+	comments = Comment.objects.all().order_by('-date')
+	form = CommentForm()
+	#return render_to_response('index.html',locals(),RequestContext(request))
+	return render_to_response('starter-template.html',locals(),RequestContext(request))
+
+class CommentForm(ModelForm):
+	class Meta:
+		model=Comment
+		widgets = {
+			'text': Textarea(attrs={'class':'span6 addCommentInput'}),
+			'user': TextInput(attrs={'class':'addCommentInput'}),
+		}
+		exclude =["blog"]
+
+def add_comment(request, pk):
+	p = request.POST
+	
+	if p.has_key("text") and p["text"]:
+		author = "Anonymous"
+		if p["user"]: author = p["user"]
+		comment = Comment(belongs_to_blog=Blog.objects.get(pk=pk))
+		cf = CommentForm(p, instance=comment)
+		cf.fields["user"].required = False
+
+		#comment = cf.save(commit=False)
+		comment.text = p["text"]
+		comment.user = author
+		comment.save()
+	return HttpResponseRedirect(reverse("blog_index"))
+#	else:
+#		return HttpResponseRedirect(reverse("blog_index",args=[p["user"]]))
+		
+def delete_comment(request,pkey):
+	p=request.POST
+	if request.user.is_staff:
+		comment=Comment.objects.get(pk=pkey)
+		comment.delete()
+	return HttpResponseRedirect(reverse("blog_index"))
